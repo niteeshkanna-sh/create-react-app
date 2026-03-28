dc079b144c6bcd5f6a86f2530427199d012ac84e
niteeshkanna-sh <niteeshniteesh045@gmail.com>
2026-03-28 11:09:35 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      animation: {
+        float: "float 4s ease-in-out infinite",
+      },
+      keyframes: {
+        float: {
+          "0%, 100%": { transform: "translateY(0px)" },
+          "50%": { transform: "translateY(-20px)" },
+        },
+      },
+    },
+  },
+  plugins: [],
+};
+
